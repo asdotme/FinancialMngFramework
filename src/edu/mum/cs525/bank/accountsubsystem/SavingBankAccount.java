0b068965095171f9ff67da8ba2aff9d7acd09b87
lasmek <asm.ece@gmail.com>
2017-02-07 17:48:01 -0600

    created CheckingAccount and SavingAccount class

---
+++ src/edu/mum/cs525/bank/accountsubsystem/SavingBankAccount.java
@@ -0,0 +1,22 @@
+package edu.mum.cs525.bank.accountsubsystem;
+
+import edu.mum.cs525.finco.accountsubsystem.controller.EvaluateFunctor;
+import edu.mum.cs525.finco.accountsubsystem.model.Account;
+import edu.mum.cs525.finco.customersubsystem.model.Customer;
+import edu.mum.cs525.finco.customersubsystem.model.ICustomer;
+
+public class SavingBankAccount extends Account {
+
+
+    public SavingBankAccount(ICustomer accountOwner, String accountNumber, double accountInterestRate, EvaluateFunctor evaluateFunctor) {
+        super(accountOwner, accountNumber, accountInterestRate, evaluateFunctor);
+    }
+
+    public SavingBankAccount(Customer accountOwner, String accountNumber, EvaluateFunctor evaluateFunctor) {
+
+        super(accountOwner, accountNumber,0.1, evaluateFunctor);
+    }
+
+    public SavingBankAccount() {
+    }
+}
---
+++ src/edu/mum/cs525/ccard/CCard.java
@@ -0,0 +1,202 @@
+package edu.mum.cs525.ccard;
+
+import edu.mum.cs525.finco.FinCo;
+import edu.mum.cs525.finco.accountsubsystem.controller.AccountController;
+import edu.mum.cs525.finco.accountsubsystem.controller.EvaluateFunctor;
+import edu.mum.cs525.finco.accountsubsystem.controller.IAccountController;
+import edu.mum.cs525.finco.accountsubsystem.controller.IAccountVisitor;
+import edu.mum.cs525.finco.accountsubsystem.model.IAccount;
+import edu.mum.cs525.finco.accountsubsystem.model.ITransaction;
+import edu.mum.cs525.finco.customersubsystem.controller.CustomerController;
+import edu.mum.cs525.finco.customersubsystem.controller.ICustomerController;
+import edu.mum.cs525.finco.customersubsystem.model.IAddress;
+import edu.mum.cs525.finco.customersubsystem.model.ICompany;
+import edu.mum.cs525.finco.customersubsystem.model.ICustomer;
+import edu.mum.cs525.finco.customersubsystem.model.IPerson;
+import edu.mum.cs525.finco.dataaccesssubsystem.DataAccessSubSystem;
+import edu.mum.cs525.finco.presentation.FinCoMainFrame;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.util.List;
+
+/**
+ * Created by asme on 2/6/17.
+ */
+public class CCard extends FinCo {
+
+    IAccountController accountController;
+    ICustomerController customerController;
+    protected DefaultTableModel defaultTableModel;
+    EvaluateFunctor evaluateFuctor;
+    String amountColumnLabel = "Amount";
+
+    
+    public CCard(IAccountController accountController, ICustomerController customerController) {
+		this.accountController = accountController;
+		this.customerController = customerController;
+	}
+
+    public CCard() {
+		this.accountController = new AccountController();
+		this.customerController= new CustomerController();
+	}
+    
+
+
+    @Override
+    public void withdrawMoney(IAccount account, ITransaction transaction) {
+        account.withdraw(transaction);
+    }
+
+    @Override
+    public void addInterest() {
+        accountController.addInterest();
+        refreshDataTableRows();
+    }
+
+    @Override
+    public String generateReport(IAccount account) {
+    	return account.generateReport();
+    }
+
+
+    public static void main(String[] args) {
+    	IAccountController acctController = new AccountController();
+    	acctController.setDbStore(new DataAccessSubSystem()); //set the database system
+        CCard finco = new CCard();
+        
+        String[] dataTableCols = {"AccountNo", "Name", "Type", "Balance"};
+        finco.initializeFincoApp(dataTableCols);
+
+    }
+
+    private void initializeFincoApp(String[] dataTableCols) {
+        //initialize defaultdatatablecolumn
+        defaultTableModel = new DefaultTableModel();
+        for (String colName : dataTableCols) {
+            defaultTableModel.addColumn(colName);
+        }
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+        this.setDefaultTableModel(defaultTableModel);
+        new FinCoMainFrame(this).setVisible(true);
+        ;
+
+    }
+
+    public DefaultTableModel getDefaultTableModel() {
+        return defaultTableModel;
+    }
+
+    public void setDefaultTableModel(DefaultTableModel defaultTableModel) {
+        this.defaultTableModel = defaultTableModel;
+    }
+
+    public void addRow(IAccount account) {
+		ICustomer customer = account.getAccountOwner();
+		IAddress address = customer.getAddress();
+		Object rowdata[] = new Object[defaultTableModel.getColumnCount()];
+		rowdata[0] = account;
+		rowdata[1] = customer.getName();
+		rowdata[2] = address.getCity();
+		rowdata[3] = customer.getType();
+		// rowdata[4] = accountType;
+		rowdata[4] = account.getAccountBalance();
+		defaultTableModel.addRow(rowdata);
+    }
+
+    @Override
+    public void depositeMoney(IAccount account, ITransaction transaction) {
+        /*int ammountColIndex = defaultTableModel.findColumn(getAmountColumnLabel());
+        double newamount = (double) defaultTableModel.getValueAt(selectedIndex, ammountColIndex) + amount;
+        defaultTableModel.setValueAt(newamount, selectedIndex, ammountColIndex); //put the value in selected row and column
+        if (amount < 0 && newamount < 0) {
+            JOptionPane
+                    .showMessageDialog(null,
+                            "No enough money in person account",
+                            "Invalid deposit attempt!"
+                            , JOptionPane.WARNING_MESSAGE);
+        }*/
+
+        //update the model and the account, push it also into the database
+
+    	account.deposite(transaction);
+
+    }
+
+    public String getAmountColumnLabel() {
+        return amountColumnLabel;
+    }
+
+    public void setAmountColumnLabel(String amountColumnLabel) {
+        this.amountColumnLabel = amountColumnLabel;
+    }
+
+    /*@Override
+    public void withdrawMoney(IAccount account, int rowIndex, double amountToWithdraw) {
+        int ammountColIndex = defaultTableModel.findColumn(getAmountColumnLabel());
+        double currentAmount = (double) defaultTableModel.getValueAt(rowIndex, ammountColIndex);
+
+        if (currentAmount < amountToWithdraw) {
+            JOptionPane
+                    .showMessageDialog(null,
+                            "No enough money to make this withdraw",
+                            "Insufficient balance!"
+                            , JOptionPane.WARNING_MESSAGE);
+        } else {
+            System.out.println("Not possible");
+        }
+    }*/
+    
+    public void refreshDataTableRows(){
+    	if (defaultTableModel.getRowCount() > 0) {
+			for (int i = defaultTableModel.getRowCount() - 1; i > -1; i--) {
+				defaultTableModel.removeRow(i);
+			}
+		}
+		List<IAccount> accounts = accountController.getAccounts();
+
+		for (IAccount account : accounts)
+			addRow(account);
+    }
+
+	/*@Override
+	public void addCustomer(ICustomer customer) {
+		int accountNumber = accountController.getAccounts().size() + 1;
+//		accountController.createCompanyAccount(, company, accountNumber);
+	}*/
+
+	public EvaluateFunctor getEvaluateFuctor() {
+		return evaluateFuctor;
+	}
+
+	public void setEvaluateFuctor(EvaluateFunctor evaluateFuctor) {
+		this.evaluateFuctor = evaluateFuctor;
+	}
+
+	@Override
+	public void addCompanyAccount(IAccountVisitor accountVisitor, ICompany company, String accountNumber) {
+		accountController.createCompanyAccount(accountVisitor, company, accountNumber);
+		
+		
+	}
+
+	@Override
+	public void addPersonAccount(IAccountVisitor accountVisitor, IPerson person, String accountNumber) {
+		accountController.createPersonAccount(accountVisitor, person, accountNumber);	
+	}
+	
+}
+
+
+
